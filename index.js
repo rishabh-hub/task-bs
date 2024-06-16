dffee6730b23298b07b2f10c38f33e3c8815fa6a
rishabh-hub <rishabh.1056@gmail.com>
2024-06-16 18:24:09 +0530

    Added ts, 2 new routes for listing users and creating a user in the table.

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-const express = require("express");
-const { PrismaClient } = require("@prisma/client");
-
+import express from "express";
+import { PrismaClient } from "@prisma/client";
 const app = express();
 const prisma = new PrismaClient();
-
 app.use(express.json());
-
+app.get("/health", async (req, res) => {
+    // const users = await prisma.contacts.findMany();
+    res.status(200).json("Healthy");
+});
+app.get("/users", async (req, res) => {
+    const users = await prisma.contacts.findMany();
+    res.status(200).json(users);
+});
+app.post("/users", async (req, res) => {
+    const { email, phoneNumber, linkedId, linkPrecedence } = req.body;
+    const user = await prisma.contacts.create({
+        data: { email, phoneNumber, linkedId, linkPrecedence },
+    });
+    res.json(user);
+});
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
---
+++ index.ts
@@ -0,0 +1,30 @@
+import express, { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+
+const app = express();
+const prisma = new PrismaClient();
+
+app.use(express.json());
+
+app.get("/health", async (req: Request, res: Response) => {
+  // const users = await prisma.contacts.findMany();
+  res.status(200).json("Healthy");
+});
+
+app.get("/users", async (req: Request, res: Response) => {
+  const users = await prisma.contacts.findMany();
+  res.status(200).json(users);
+});
+
+app.post("/users", async (req: Request, res: Response) => {
+  const { email, phoneNumber, linkedId, linkPrecedence } = req.body;
+  const user = await prisma.contacts.create({
+    data: { email, phoneNumber, linkedId, linkPrecedence },
+  });
+  res.json(user);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
